cc1418fd2df2277d842a84a365b23de903561d89
trample <trample68@163.com>
2012-06-07 00:53:52 -0400

    recreate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpyGame::Application.config.secret_token = 'f01e4a3f15bcf93976248fe582a1f47cf5db15e2f9f493c7d18a42d273cb349ed5df0047e0627ce5b7f3c141b26cee71dfc71b4b86813ed7b1fa148c65cefdb7'
