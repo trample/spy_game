4cf3545f6f6b6a46cbafeef07a7a8b2282b136b4
trample <trample68@163.com>
2012-06-07 08:37:26 -0400

    master empty branch

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,3 @@
 class Mission < ActiveRecord::Base
   attr_accessible :instruction, :user_id
-  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :password
-  validates :password, :length => { :minimum => 5 }
-  has_many :missions
+
 end
